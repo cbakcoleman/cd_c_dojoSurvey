73acba3ec3483641ce063dbfb7a02d0891827d5c
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-30 20:56:48 -0700

    complete form, add results and route

--- Controllers/Survey.cs
+++ Controllers/Survey.cs
@@ -4,14 +4,24 @@ namespace cd_c_dojoSurvey.Controllers
 {
     public class SurveyController : Controller
     {
-        [Route("")]
-        [HttpGet]
+        
+        [HttpGet("")]
         public ViewResult Survey()
         {
             return View();
         } // END SURVEY ROUTE
 
         
-    }
+        [HttpPost("results")]
+        public ViewResult Results(string name, string location, string language, string comments)
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = language;
+            ViewBag.Comments = comments;
+            return View();
+        } // END RESULTS ROUTE
+        
+    } // END SURVEYCONTROLLER
 
 }
